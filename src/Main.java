5cb7dd95461f2e2490a79130ecb052f8cd5d6561
Anton Saltanov <saltanov.anton@yandex.ru>
2022-12-04 00:28:36 +0900

    Условные операторы ч1. Задача 1

---
+++ src/Main.java
@@ -0,0 +1,12 @@
+public class Main {
+    public static void main(String[] args) {
+        int age = 17;
+        if (age >= 18) {
+            System.out.println("Поздравляем вас с совершеннолетием!!!");
+        }
+
+        if (age < 18) {
+            System.out.println("Вас еще рано поздравлять, подождите немного");
+        }
+    }
+}
