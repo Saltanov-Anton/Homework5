3964234aafd91d304f74c383621ce2ecca8bc630
Anton Saltanov <saltanov.anton@yandex.ru>
2022-12-04 00:38:06 +0900

    Условные операторы ч1. Задача 2

--- src/Main.java
+++ src/Main.java
@@ -8,5 +8,18 @@ public class Main {
         if (age < 18) {
             System.out.println("Вас еще рано поздравлять, подождите немного");
         }
+
+        int age2 = 25;
+        if (age2 >= 7 & age2 < 18) {
+            System.out.println("Вы ходите в школу");
+        }
+
+        if (age2 >= 18 & age2 < 24) {
+            System.out.println("Вам пора в университет");
+        }
+
+        if (age2 >= 24) {
+            System.out.println("Вам пора на работу");
+        }
     }
 }
